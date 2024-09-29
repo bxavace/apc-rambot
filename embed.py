c6cf9f7d516697862b4f1db6add64621415ef4d4
brylle <b_xavace@outlook.com>
2024-09-29 16:36:13 +0800

    new: embeddings model and index

---
+++ embed.py
@@ -0,0 +1,32 @@
+import os
+from langchain_community.vectorstores.azuresearch import AzureSearch
+from langchain_openai import AzureOpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AZ_OAI_ENDPOINT = os.getenv('AZ_OAI_ENDPOINT')
+AZ_OAI_VER = os.getenv('AZ_OAI_VER')
+AZ_OAI_KEY = os.getenv('AZ_OAI_KEY')
+VECTOR_STORE_ADDR= os.environ.get('VECTOR_STORE_ADDR')
+VECTOR_STORE_KEY= os.environ.get('VECTOR_STORE_KEY')
+AZ_OAI_DEPLOYMENT= os.environ.get('AZ_OAI_DEPLOYMENT')
+
+embeddings = AzureOpenAIEmbeddings(
+    azure_deployment=AZ_OAI_DEPLOYMENT,
+    openai_api_version=AZ_OAI_VER,
+    azure_endpoint=AZ_OAI_ENDPOINT,
+    api_key=AZ_OAI_KEY,
+)
+
+datastore = AzureSearch(
+    azure_search_endpoint=VECTOR_STORE_ADDR,
+    azure_search_key=VECTOR_STORE_KEY,
+    index_name="apc-index-qa",
+    embedding_function=embeddings.embed_query,
+)
+
+retriever = datastore.as_retriever()
+
+if __name__ == "__main__":
+    pass
