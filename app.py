346a5ac8073eae7a2f2ec46d1653529b7a4c38a8
brylle <b_xavace@outlook.com>
2024-10-04 09:54:57 +0800

    add: working dev db schema

--- app.py
+++ app.py
@@ -3,10 +3,19 @@ from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Development, Production
+from chat import gpt4om
+from dotenv import load_dotenv
+
+import os
+import threading
+
+load_dotenv()
 
 app = Flask(__name__)
 
-if app.env == 'development':
+env = os.getenv('FLASK_ENV', 'development')
+
+if env == 'development':
     app.config.from_object(Development)
 else:
     app.config.from_object(Production)
@@ -15,32 +24,45 @@ db = SQLAlchemy(app)
 api = Api(app)
 migrate = Migrate(app, db)
 
+class Conversation(db.Model):
+    __tablename__ = 'conversations'
+    id = db.Column(db.Integer, primary_key=True)
+    user_message = db.Column(db.Text, nullable=False)
+    bot_response = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, server_default=db.func.now())
+    
+    def __repr__(self):
+        return f'<Conversation {self.id}>'
+
 class GreetTest(Resource):
     def get(self):
-        return jsonify({'message': 'Server operational'})
+        return jsonify({'message': 'Everything is absurd. And since everything is absurd, we should try to live as happily as possible.'})
 
 class Chatbot(Resource):
     """
         Main endpoint of the API.
-        This will call '/api/v1/save' to save the user message and bot response.
+        Update: The saving of the conversation is now done in a separate thread under one resource.
     """
     def post(self):
-        user_message = request.json
-        response = ""
-        return jsonify({'response': response})
+        data = request.json
+        user_message = data.get('user_message')
+        response = gpt4om.invoke(user_message)
 
-class SaveMessage(Resource):
-    """
-        Logs the user message and bot response.
-    """
-    def post(self):
-        return jsonify({'message': 'User message saved.'})
+        threading.Thread(target=save_message, args=(user_message, response.content)).start()
+
+        return jsonify({'response': str(response.content)})
+
+def save_message(user_message, bot_response):
+    with app.app_context():
+        conversation = Conversation(user_message=user_message, bot_response=bot_response)
+        db.session.add(conversation)
+        db.session.commit()
+        print('Conversation saved.')
 
 with app.app_context():
     db.create_all()
 
 api.add_resource(GreetTest, '/test')
-api.add_resource(SaveMessage, '/api/v1/save')
 api.add_resource(Chatbot, '/api/v1/chat')
 
 if __name__ == '__main__':
