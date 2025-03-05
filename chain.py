298fa1c75e9989e04be75b0acb8cd2ced4c2fb6e
bxavace <b_xavace@outlook.com>
2025-03-05 08:19:45 +0800

    feat: enable streaming responses in chat and format output as markdown

--- chain.py
+++ chain.py
@@ -8,6 +8,7 @@ from langchain.chains.history_aware_retriever import create_history_aware_retrie
 
 from embed import retriever
 from datetime import datetime
+import markdown
 
 today = datetime.today()
 date = today.strftime("%B %d, %Y")
@@ -90,6 +91,11 @@ qa_prompt = ChatPromptTemplate.from_messages(
 question_answer_chain = create_stuff_documents_chain(gpt4om, qa_prompt)
 
 rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+async def generate_response(message):
+    async for chunk in rag_chain.astream(message):
+        content = markdown.markdown(chunk)
+        yield content
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
